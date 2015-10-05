abf90ede8ca9dd8b29cf453ba25fced268a4ed65
John D <redline@Johns-MacBook-Air.local>
2015-10-04 23:14:24 -0500

    Changed braces

--- letterChanges.js
+++ letterChanges.js
@@ -1,79 +1,62 @@
 (function(){
 
-   function isLetter(unicode)
-   {
+   function isLetter(unicode) {
       var aUnicode = 'a'.charCodeAt(0);
       var zUnicode = 'z'.charCodeAt(0);
-      if(unicode >= aUnicode && unicode <= zUnicode)
-      {
+      if(unicode >= aUnicode && unicode <= zUnicode) {
          return true;
       }
-      else
-      {
+      else {
          return false;
       }
    }
 
-   function nextCharacter(character)
-   {
+   function nextCharacter(character) {
       var unicodeOfCharacter = character.charCodeAt(0);
-      if(isLetter(unicodeOfCharacter))
-      {
-         if(character === 'z')
-         {
+      if(isLetter(unicodeOfCharacter)) {
+         if(character === 'z') {
             return 'a';
          }
-         else
-         {
+         else {
             var nextUnicode = unicodeOfCharacter + 1;
             return String.fromCharCode(nextUnicode);
          }
       }
-      else
-      {
+      else {
          return character;
       }
    }
 
-   function isVowel(character)
-   {
+   function isVowel(character) {
       var allVowels = ['a', 'e', 'i', 'o', 'u'];
-      if(allVowels.indexOf(character) !== -1)
-      {
+      if(allVowels.indexOf(character) !== -1) {
          return true;
       }
-      else
-      {
+      else {
          return false;
       }
    }
 
-   function capitalize(character)
-   {
+   function capitalize(character) {
       var unicodeOfCharacter = character.charCodeAt(0);
       var unicodeOfCapitalCharacter = unicodeOfCharacter - 32;
       return String.fromCharCode(unicodeOfCapitalCharacter);
    }
 
-   function capitalizeVowels(character)
-   {
-      if(isVowel(character))
-      {
+   function capitalizeVowels(character) {
+      if(isVowel(character)) {
          return capitalize(character);
       }
-      else
-      {
+      else {
          return character;
       }
    }
 
-   function joinCharacters(previous, next)
-   {
+   function joinCharacters(previous, next) {
       return previous.concat(next);
    }
 
-   function applyLetterChanges(str)
-   {
+   function applyLetterChanges(str) {
       var charArray = str.split("");
       var changedStr =
          _(charArray).chain()
